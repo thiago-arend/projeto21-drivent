0cb7b191a55280ff912de753a119f0e2ebd54fb9
thiago-arend <thiago.arend@gmail.com>
2023-10-01 18:49:05 -0300

    fix: ordem deleção reset testes

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -11,9 +11,12 @@ export async function cleanDb() {
   await prisma.ticketType.deleteMany({});
   await prisma.enrollment.deleteMany({});
   await prisma.event.deleteMany({});
+  await prisma.booking.deleteMany({});
+  await prisma.room.deleteMany({});
+  await prisma.hotel.deleteMany({});
   await prisma.session.deleteMany({});
   await prisma.user.deleteMany({});
-  await prisma.hotel.deleteMany({});
+  
 }
 
 export async function generateValidToken(user?: User) {
