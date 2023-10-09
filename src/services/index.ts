ae5e1e76f09c0a772166ee5a113cfa95ad096428
thiago-arend <thiago.arend@gmail.com>
2023-10-09 15:28:50 -0300

    fix: nomes dos arquivos no singular (exigencia do hub)

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,4 +5,4 @@ export * from './enrollments-service';
 export * from './tickets-service';
 export * from './payments-service';
 export * from './hotels-service';
-export * from './bookings-service';
+export * from './booking-service';
